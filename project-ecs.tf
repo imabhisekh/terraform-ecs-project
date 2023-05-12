11cc43df4b757d4c3e0f4c09895497207e6fa257
imabhisekh <abhishektembhare72@gmail.com>
2023-05-13 01:32:34 +0530

    lobbb

--- project-ecs.tf
+++ project-ecs.tf
@@ -16,21 +16,22 @@ data "template_file" "cb_app" {
 }
 
 resource "aws_ecs_task_definition" "app" {
-    family                   = "abhi-app-task"
+    family                   = "testing my repo"
     execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
     network_mode             = "awsvpc"
     requires_compatibilities = ["FARGATE"]
     cpu                      = var.fargate_cpu
     memory                   = var.fargate_memory
     container_definitions    = data.template_file.cb_app.rendered
+
 }
 
 resource "aws_ecs_service" "main" {
-    name            = "abhi-service1"
+    name            = "i dont know1"
     cluster         = aws_ecs_cluster.main.id
     task_definition = aws_ecs_task_definition.app.arn
     desired_count   = var.app_count
-    launch_type     = "FARGATE"
+    launch_type     = "FARGATlE"
 
     network_configuration {
         security_groups  = [aws_ecs_cluster.main.id]
